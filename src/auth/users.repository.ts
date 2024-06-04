bf66058e4aa5dd0e457cd88339e1ab555ca4660f
TomasFernandez1 <tomasefernandez1@gmail.com>
2024-06-04 16:21:21 -0300

    Lesson 64

--- src/auth/users.repository.ts
+++ src/auth/users.repository.ts
@@ -5,7 +5,7 @@ import {
   InternalServerErrorException,
 } from '@nestjs/common';
 
-import bcrypt from 'bcrypt';
+import * as bcrypt from 'bcrypt';
 
 import { User } from './user.entity';
 import { AuthCredentialsDto } from './dto/auth-credentials.dto';
@@ -18,13 +18,14 @@ export class UsersRepository extends Repository<User> {
 
   async createUser(authCredentialsDto: AuthCredentialsDto): Promise<void> {
     const { username, password } = authCredentialsDto;
-    const user = this.create({
-      username,
-      password,
-    });
     const salt = await bcrypt.genSalt();
     const hashedPassword = await bcrypt.hash(password, salt);
-    
+
+    const user = this.create({
+      username,
+      password: hashedPassword,
+    });
+
     try {
       await this.save(user);
     } catch (error) {
